895861066fcc314b11532cb9e441febf31563622
Akriti Khadka <akritikhadka679@gmail.com>
2020-06-19 10:47:47 +0545

    changes made in map1.py

--- map1.py
+++ map1.py
@@ -1,3 +1,6 @@
 import folium
-map=folium.Map(location=[38.58,-99.09],zoomstart=6)
+map=folium.Map(location=[38.58,-99.09],zoomstart=6,tiles="Mapbox Bright")
+fg=folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.58,-99.09],popup="Hi aaku",icon=folium.Icon(color='green')))
+map.add_child(fg)
 map.save("Map1.html")
